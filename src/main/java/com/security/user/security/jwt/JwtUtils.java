e5c59d09b1f09601df4edcd2c42f87e8f8478d6e
emendoza96 <emilianomendoza294@gmail.com>
2024-02-21 14:28:33 -0300

    Add jwt utils

--- src/main/java/com/security/user/security/jwt/JwtUtils.java
+++ src/main/java/com/security/user/security/jwt/JwtUtils.java
@@ -1,7 +1,15 @@
 package com.security.user.security.jwt;
 
+import java.security.Key;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtUtils {
@@ -12,4 +20,30 @@ public class JwtUtils {
     @Value("${jwt.time.expiration}")
     private String timeExpiration;
 
+    public String generateAccessToken(String username) {
+        return Jwts.builder()
+            .setSubject(username)
+            .setIssuedAt(new Date(System.currentTimeMillis()))
+            .setExpiration(new Date(System.currentTimeMillis() + Long.parseLong(timeExpiration)))
+            .signWith(getSignatureKey(), SignatureAlgorithm.HS256)
+            .compact();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            Jwts.parserBuilder()
+                .setSigningKey(getSignatureKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Key getSignatureKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
